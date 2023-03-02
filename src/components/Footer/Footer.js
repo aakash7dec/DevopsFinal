e2762229f09b64d45b0566ec374b0e5dd1f84c9a
aakash7dec <aakash107raj@gmail.com>
2023-03-02 11:25:37 +0530

    fb

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <div className="footer-content">
           <div className="footer-section about">
             <p className="footer-para">
-              Developer: <span className="my-name"><a href ="0 @https://www.instagram.com/mohan_2808_/</span>">Alpha_roy punde ku </a></span>
+              Developer: <span className="my-name"><a href ="https://www.instagram.com/mohan_2808_/</span>">Alpha_roy punde ku </a></span>
             </p>
 
             <div className="contact"></div>
@@ -17,7 +17,7 @@ function Footer() {
               <a
                 href="https://www.facebook.com/profile.php?id=100041078220896"
                 target="_blank"
-                rel="noopener noreferrer"
+                rel="noopener noreferrer" 
               >
                 <i className="fab fa-facebook"></i>
               </a>
