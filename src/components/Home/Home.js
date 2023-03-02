d6b6092168f30b411d939711b2323fd4f34be12b
aakash7dec <aakash107raj@gmail.com>
2023-03-02 10:06:49 +0530

    changes made in about page
    spelling mistake

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -34,6 +34,9 @@ function Home() {
                   />
                   <h3 className="shoe-name">{shoe.name} AirJordans</h3>
                   <h3 className="shoe-price">${shoe.price}40000 </h3>
+                  <h3 className="shoe-name">{shoe.name} NikeStud</h3>
+                  <h3 className="shoe-price">${shoe.price}35000 </h3>
+                
                 </div>
               );
             })}
